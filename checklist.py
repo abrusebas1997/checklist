5dcc71b066daab7b7c554da2966d1b20c47514a1
Sebastian Abarca <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-09-03 10:33:14 -0700

    Second commit

--- checklist.py
+++ checklist.py
@@ -2,11 +2,14 @@ checklist = list()
 # CREATE
 def create(item):
     checklist.append(item)
+    checklist[0]
+
 
 # READ
 def read(index):
-    item = checklist[index]
-    return item
+    print(checklist[index])
+    return checklist[index]
+
 
 # UPDATE
 def update(index, item):
@@ -20,44 +23,65 @@ def destroy(index):
 def list_all_items():
     index = 0
     for list_item in checklist:
-        # print(str(index) + list_item)
         print("{} {}".format(index, list_item))
         index += 1
-#TO MARK COMPLLETED WITH "√"
+# TO MARK COMPLLETED WITH "√"
 def mark_completed(index):
-    checklist[(index)] = "{} {}".format("√", checklist[(index)])
+    checklist[(index)] = "{} {}".format("√", checklist[int(index)])
+
 
 def select(function_code):
     # Create item
     if function_code == "C":
         input_item = user_input("Input item:")
         create(input_item)
-
     # Read item
     elif function_code == "R":
-        item_index = user_input("Index Number?")
-
-        # Remember that item_index must actually exist or our program will crash.
+        item_index = int(user_input("Index Number?"))
+    # Remember that item_index must actually exist or our program will crash.
         read(item_index)
-
     # Print all items
     elif function_code == "P":
         list_all_items()
-
+    # Stop the loop
+    elif function_code == "Q":
+         return False
+         
     # Catch all
     else:
         print("Unknown Option")
-# # TEST
-# def test():
-#     # Your testing code here
-#     # ...
-#     # Call your new function with the appropriate value
-#     select("C")
-#     # View the results
-#     list_all_items()
-#     # Call function with new value
-#     select("R")
-#     # View results
-#     list_all_items()
-#     # Continue until all code is run
-# test()
+
+    return True
+
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
+# TEST
+def test():
+    # First Test Code
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    list_all_items()
+    # New Test Code 2
+    select("C")
+    list_all_items()
+    select("R")
+    list_all_items()
+
+    user_value = user_input("Please Enter a value:")
+    print(user_value)
+
+test()
+running = True
+while running:
+    selection = user_input("Press C to add to list, R to Read from list and P to display list, and Q to quit: ")
+    running = select(selection)
