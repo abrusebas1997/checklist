2168845bb6a694bfe570b2c941b589dfb250cf73
Sebastian Abarca <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-09-16 13:05:43 -0700

    fixed

--- checklist.py
+++ checklist.py
@@ -13,11 +13,11 @@ def read(index):
 
 # UPDATE
 def update(index, item):
-    checklist[index] = item
+    checklist[int(index)] = str(item)
 
 # DESTROY
 def destroy(index):
-    checklist.pop(index)
+    checklist.pop(int(index))
 
 # TO LIST EVERY ITEM IN THE LIST
 def list_all_items():
@@ -32,25 +32,39 @@ def mark_completed(index):
 
 def select(function_code):
     # Create item
-    if function_code == "C":
-        input_item = user_input("Input item:")
+    if function_code.upper() == "C":
+        input_item = user_input('\033[31m' +"Input item: ")
         create(input_item)
+    # Delete item
+    elif function_code.upper() == "D":
+        item_index = user_input("Index number?: ")
+        destroy(item_index)
+
+     # Update item
+    elif function_code.upper() == "U":
+        item_index = user_input("Index number?: ")
+        input_item = user_input("Input Item: ")
+        update(item_index, input_item)
+
+       # Mark as complete
+    elif function_code.upper() == "M":
+        item_index = user_input("Index number?: ")
+        mark_completed(item_index)
     # Read item
-    elif function_code == "R":
-        item_index = int(user_input("Index Number?"))
+    elif function_code.upper() == "R":
+        item_index = int(user_input("Index Number?: "))
     # Remember that item_index must actually exist or our program will crash.
-        read(item_index)
+        read(int(item_index))
     # Print all items
-    elif function_code == "P":
+    elif function_code.upper() == "P":
         list_all_items()
     # Stop the loop
-    elif function_code == "Q":
+    elif function_code.upper() == "Q":
          return False
-         
+
     # Catch all
     else:
-        print("Unknown Option")
-
+        print("\033[31m" +"Unknown Option")
     return True
 
 def user_input(prompt):
@@ -58,30 +72,30 @@ def user_input(prompt):
     return user_input
 
 # TEST
-def test():
-    # First Test Code
-    create("purple sox")
-    create("red cloak")
-
-    print(read(0))
-    print(read(1))
-
-    update(0, "purple socks")
-    destroy(1)
-
-    print(read(0))
-    list_all_items()
-    # New Test Code 2
-    select("C")
-    list_all_items()
-    select("R")
-    list_all_items()
-
-    user_value = user_input("Please Enter a value:")
-    print(user_value)
-
-test()
+# def test():
+#     # First Test Code
+#     create("purple sox")
+#     create("red cloak")
+#
+#     print(read(0))
+#     print(read(1))
+#
+#     update(0, "purple socks")
+#     destroy(1)
+#
+#     print(read(0))
+#     list_all_items()
+#     # New Test Code 2
+#     select("C")
+#     list_all_items()
+#     select("R")
+#     list_all_items()
+#
+#     user_value = user_input("Please Enter a value:")
+#     print(user_value)
+#
+# test()
 running = True
 while running:
-    selection = user_input("Press C to add to list, R to Read from list and P to display list, and Q to quit: ")
+    selection = user_input("Press C to add to list, R to Read from list, U to update list item, D to delete list item, P to display list, and Q to quit: ")
     running = select(selection)
